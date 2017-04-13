6850e8a96be9edb5232e96a268388546794ac6e6
nagboy@gmail.com <nagboy@gmail.com@78e66ff5-1fef-4877-a307-636d9c162e3d>
2017-04-13 02:26:08 +0000

    Fixed L thingy

--- trunk/locales/zhTW.lua
+++ trunk/locales/zhTW.lua
@@ -1,3 +1,4 @@
 local L = LibStub("AceLocale-3.0"):NewLocale("HandyNotes_DungeonLocations", "zhTW")
+if not L then return end
 
 --@localization(locale="zhTW", format="lua_additive_table", same-key-is-true=true, handle-subnamespaces="concat")@
